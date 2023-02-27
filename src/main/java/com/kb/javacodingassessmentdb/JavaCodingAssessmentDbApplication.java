ba133bba18aa8903f38492feb6dc2bd752729046
Karim <karimb.kb@gmail.com>
2023-02-27 23:56:20 +0100

    Initialize spring boot service

---
+++ src/main/java/com/kb/javacodingassessmentdb/JavaCodingAssessmentDbApplication.java
@@ -0,0 +1,13 @@
+package com.kb.javacodingassessmentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingAssessmentDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodingAssessmentDbApplication.class, args);
+    }
+
+}
